c20f287ff2e1b05727879409fbe7a5e4cd5550d8
nicklhw <nicklhw@gmail.com>
2022-09-14 14:13:51 -0400

    minor cosmetic updates

--- docker-compose/scripts/init_c4.sh
+++ docker-compose/scripts/init_c4.sh
@@ -6,16 +6,16 @@ export VAULT_INIT_OUTPUT=vault_c4.json
 export VAULT_SKIP_VERIFY=true
 
 # Init vault_c4_s1
-echo "Init and unseal vault_c4_s1"
+tput setaf 11 && echo "*** Init vault_c4_s1"
 export VAULT_ADDR=https://localhost:48201
 sleep 5
 vault operator init -format=json -n 1 -t 1 > ${VAULT_INIT_OUTPUT}
 
 export VAULT_TOKEN=$(cat ${VAULT_INIT_OUTPUT} | jq -r '.root_token')
-echo "Root VAULT TOKEN is: $VAULT_TOKEN"
+tput setaf 11 && echo "*** Root VAULT TOKEN is: $VAULT_TOKEN"
 
 # Unseal vault_c4_s1
-echo "Unseal vault_c4_s1"
+tput setaf 11 && echo "*** Unseal vault_c4_s1"
 export VAULT_ADDR=https://localhost:48201
 
 export unseal_key=$(cat ${VAULT_INIT_OUTPUT} | jq -r '.unseal_keys_b64[0]')
@@ -24,21 +24,21 @@ vault operator unseal ${unseal_key}
 sleep 5
 
 # Join vault_c4_s2
-echo "Join vault_c4_s2"
+tput setaf 11 && echo "*** Join vault_c4_s2"
 export VAULT_ADDR=https://localhost:48202
 vault operator raft join -leader-ca-cert=@../vault_c4/vault_c4_s2/vault_ca.crt https://${VAULT_c4_S1_DNS}:8200
 
 # Unseal vault_c4_s2
-echo "Unseal vault_c4_s2"
+tput setaf 11 && echo "*** Unseal vault_c4_s2"
 vault operator unseal ${unseal_key}
 
 # Join vault_c4_s3
-echo "Join vault_c4_s3"
+tput setaf 11 && echo "*** Join vault_c4_s3"
 export VAULT_ADDR=https://localhost:48203
 vault operator raft join -leader-ca-cert=@../vault_c4/vault_c4_s3/vault_ca.crt https://${VAULT_c4_S1_DNS}:8200
 
 # Unseal vault_c4_s3
-echo "Unseal vault_c4_s3"
+tput setaf 11 && echo "*** Unseal vault_c4_s3"
 vault operator unseal ${unseal_key}
 
 # Reset vault addr and add vault token
@@ -46,10 +46,8 @@ export VAULT_ADDR=https://localhost:48201
 
 sleep 5
 
-echo "*** Cluster members ***"
+tput setaf 11 && echo "*** Vault_C4 Cluster members"
 vault operator members
 
 export VAULT_TOKEN=$(cat ${VAULT_INIT_OUTPUT} | jq -r '.root_token')
 #vault token lookup
-
-echo "*** Please Run: export VAULT_TOKEN=${VAULT_TOKEN}"
